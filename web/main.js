7eea2cacfea64dd3546a07f9799e2c38508b88bb
Bowen Yu <yubowenok@gmail.com>
2014-04-18 05:26:34 -0400

    initial page

---
+++ web/main.js
@@ -0,0 +1,14 @@
+$(document).ready( function(){
+	$("#user").button().addClass("btn-text");
+	$("#submit").button().addClass("submitbtn");
+	/*
+	$("td").addClass("ui-widget-content");
+	$("tr")
+		.hover( function(){ $(this).children("td").addClass("ui-state-hover"); },
+				function(){ $(this).children("td").removeClass("ui-state-hover"); } )
+		.click( function(){ 
+			$(this).children("td").toggleClass("ui-state-highlight");
+			$(this).children("td").toggleClass("ui-state-highlight");
+		});
+		*/
+});
